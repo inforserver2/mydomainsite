7eaee61f63a23025b69e5c04aef3308d66292f51
Your Name <you@example.com>
2013-12-25 13:33:33 -0300

    assets

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,26 +3,18 @@ set :repo_url, 'git@github.com:inforserver2/mydomainsite.git'
 
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
-#set :user, "max"
-#set :group, "max"
-#set :use_sudo, false
 set :deploy_to, '/var/web/uol.com'
 set :scm, :git
 
-set :deploy_via, :copy
-set :copy_strategy, :export
-
-
-
-# set :format, :pretty
-# set :log_level, :debug
-# set :pty, true
+set :format, :pretty
+set :log_level, :debug
+set :pty, true
 
 # set :linked_files, %w{config/database.yml}
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
- set :keep_releases, 5
+set :default_env, { path: "/opt/ruby/bin:$PATH" }
+set :keep_releases, 5
 
 namespace :deploy do
 
@@ -30,7 +22,7 @@ namespace :deploy do
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
+      execute :touch, release_path.join('tmp/restart.txt')
     end
   end
 
